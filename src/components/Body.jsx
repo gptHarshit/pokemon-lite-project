520fe9acee0a659c02c1df7525337fd9f6a30799
Harshit Gupta <gupta.harshit0699@gmail.com>
2026-05-01 01:17:54 +0530

    Added Pagination logic also changed url and added buttons

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,10 +3,14 @@ import PokemonCard from "./PokemonCard";
 
 const Body = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(0);
+  const offset = page * 20;
 
   const fetchPokemon = async () => {
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`,
+      );
       const Data = await response.json();
       setPokemons(Data.results);
       console.log(Data);
@@ -16,7 +20,7 @@ const Body = () => {
   };
   useEffect(() => {
     fetchPokemon();
-  }, []);
+  }, [page]);
 
   return (
     <>
@@ -26,6 +30,22 @@ const Body = () => {
             return <PokemonCard poke={poke} />;
           })}
         </div>
+      </div>
+      <div className="flex justify-center gap-5 mt-4">
+        <button
+          className="bg-blue-600 px-5 py-1 rounded-2xl border text-white"
+          disabled={page === 0}
+          onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+        >
+          Prev
+        </button>
+
+        <button
+          className="bg-blue-600 px-5 py-1 rounded-2xl border text-white"
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
       </div>
     </>
   );
